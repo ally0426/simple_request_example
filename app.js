be18a50b94746d3469eff88cfada057e872c3a16
Parker Lewis <parker.lewis14@att.net>
2016-04-05 11:35:40 -0700

    initial setup code for students complete

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require("express"),
+	app = express(),
+	bodyParser = require("body-parser"),
+	request = require("request"),
+	requestUrl = "https://fs-student-roster.herokuapp.com/students";
+
+require("locus");
+app.set("view engine", "jade");
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.get("/", function(req,res){
+  res.render("index");
+});
+
+app.post('/students', function(req,res){
+	// request code here
+	
+});
+
+app.get("/success", function(req, res){
+	res.render("success");
+});
+
+app.get("*", function(req,res){
+  res.render("failure");
+});
+
+app.listen(3000, function(){
+  console.log("Server is listening on port 3000");
+});
